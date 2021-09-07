43836f54bdcf03c69383d60d23975925493fcd9a
Guetti <gustavo.szigethi@alumnos.ucn.cl>
2021-09-07 00:34:00 -0300

    Pesca no tan milagrosa agregada

--- PPA-El-18/Assets/Scripts/Floater.cs
+++ PPA-El-18/Assets/Scripts/Floater.cs
@@ -2,25 +2,32 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[RequireComponent(typeof(Rigidbody))]
 public class Floater : MonoBehaviour
 {
     private Rigidbody _rigidbody;
-    private float depthBeforeSumerged = 1f;
-    private float displacementAmount = 3f;
+    private float _depthBeforeSumerged = 1f;
+    private float _displacementAmount = 3f;
+    private int _floaterCount = 1;
+    private float _waterDrag = 0.99f;
+    private float _waterAngularDrag = 0.5f;
 
     private void Awake()
     {
-        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody = GetComponentInParent<Rigidbody>();
     }
 
     private void FixedUpdate()
     {
+        _rigidbody.AddForceAtPosition(Physics.gravity / _floaterCount, transform.position, ForceMode.Acceleration);
         if (transform.position.y < 0f)
         {
             float displacementMultiplier =
-                Mathf.Clamp01(-transform.position.y / depthBeforeSumerged) * displacementAmount;
-            _rigidbody.AddForce(new Vector3(0f, Mathf.Abs(Physics.gravity.y)* displacementMultiplier, 0f), ForceMode.Acceleration);
+                Mathf.Clamp01(-transform.position.y / _depthBeforeSumerged) * _displacementAmount;
+            _rigidbody.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y)* displacementMultiplier, 0f),transform.position, ForceMode.Acceleration);
+            _rigidbody.AddForce(displacementMultiplier * -_rigidbody.velocity * _waterDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            _rigidbody.AddTorque(displacementMultiplier * -_rigidbody.angularVelocity * _waterAngularDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+
         }
     }
 }
+ 
