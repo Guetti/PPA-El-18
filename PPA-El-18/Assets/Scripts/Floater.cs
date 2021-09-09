9a1fd7e6ba848c44de7203bb54532a0364457860
Guetti <gustavo.szigethi@alumnos.ucn.cl>
2021-09-08 21:38:59 -0300

    Subido: Piscina cuadrada

--- PPA-El-18/Assets/Scripts/Floater.cs
+++ PPA-El-18/Assets/Scripts/Floater.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class Floater : MonoBehaviour
 {
     private Rigidbody _rigidbody;
-    private float _depthBeforeSumerged = 1f;
-    private float _displacementAmount = 3f;
-    private int _floaterCount = 1;
-    private float _waterDrag = 0.99f;
-    private float _waterAngularDrag = 0.5f;
+    [SerializeField]private float _depthBeforeSumerged = 1f;
+    [SerializeField]private float _displacementAmount = 3f;
+    [SerializeField]private int _floaterCount = 1;
+    [SerializeField]private float _waterDrag = 0.99f;
+    [SerializeField]private float _waterAngularDrag = 0.5f;
 
     private void Awake()
     {
@@ -18,11 +18,12 @@ public class Floater : MonoBehaviour
 
     private void FixedUpdate()
     {
+        var waveHeight = WaveManager.Instance.GetWaveHeight(transform.position.x);
         _rigidbody.AddForceAtPosition(Physics.gravity / _floaterCount, transform.position, ForceMode.Acceleration);
-        if (transform.position.y < 0f)
+        if (transform.position.y < waveHeight)
         {
             float displacementMultiplier =
-                Mathf.Clamp01(-transform.position.y / _depthBeforeSumerged) * _displacementAmount;
+                Mathf.Clamp01((waveHeight-transform.position.y) / _depthBeforeSumerged) * _displacementAmount;
             _rigidbody.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y)* displacementMultiplier, 0f),transform.position, ForceMode.Acceleration);
             _rigidbody.AddForce(displacementMultiplier * -_rigidbody.velocity * _waterDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
             _rigidbody.AddTorque(displacementMultiplier * -_rigidbody.angularVelocity * _waterAngularDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
---
+++ PPA-El-18/Assets/Scripts/WaveManager.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    public static WaveManager Instance;
+    [SerializeField] private float _amplitude = 1f;
+    [SerializeField] private float _lenght = 2f;
+    [SerializeField] private float _speed = 1f;
+    [SerializeField] private float _offset = 0f;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    private void Update()
+    {
+        _offset += Time.deltaTime * _speed;
+    }
+
+    public float GetWaveHeight(float x)
+    {
+        return _amplitude * Mathf.Sin(x / _lenght + _offset);
+    }
+}
