9996a475149d388f7857ea1a541e58861342415d
Yo o <joel.huillca@alumnos.ucn.cl>
2021-10-20 16:02:35 -0300

    Musica de fondo + configuracion

---
+++ PPA-El-18/Assets/Integrantes/JOel H,/LogicaVolumen.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogicaVolumen : MonoBehaviour
+{
+
+    public Slider slider;
+    public float sliderValue;
+    public Image imagenMute;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("volumenAudio", 0.5f);
+        AudioListener.volume = slider.value;
+        RevisarSiEstoyMute();
+    }
+    public void ChangeSlider(float valor)
+    {
+        sliderValue = valor;
+        
+        PlayerPrefs.SetFloat("volumenAudio",sliderValue);
+        AudioListener.volume = slider.value;
+        RevisarSiEstoyMute();
+    }
+
+    public void RevisarSiEstoyMute()
+    {
+        if (sliderValue == 0)
+        {
+            imagenMute.enabled = true;
+        }
+        else
+        {
+            imagenMute.enabled = false;
+        }
+    }
+}
