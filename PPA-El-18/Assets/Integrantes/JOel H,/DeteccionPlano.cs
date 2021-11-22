6f02c3213b6c28411e954e0d3fc101130a530ab0
Yo o <joel.huillca@alumnos.ucn.cl>
2021-11-22 01:18:10 -0300

    No es la guitarra lo que alegra o ahuyenta el miedo en la medianoche No es su bordón redondo y manso como el ojo de un buey No es la mano que roza o se aferra a las cuerdas buscando los sonidos sino la voz humana cuando canta y propaga los ensueños del hombre.
    La voz (Herberto Padilla)

--- PPA-El-18/Assets/Integrantes/JOel H,/DeteccionPlano.cs
+++ PPA-El-18/Assets/Integrantes/JOel H,/DeteccionPlano.cs
@@ -10,6 +10,7 @@ using UnityEngine.XR.ARSubsystems;
 public class DeteccionPlano : MonoBehaviour
 {
     public ARRaycastManager aRRaycastManager;
+
     public GameObject indicator;
     
     public GameObject objectPrefab; //++
@@ -25,21 +26,25 @@ public class DeteccionPlano : MonoBehaviour
     //Detección de Planos
     public void PlaneDetection()
     {
-        List<ARRaycastHit> hits = new List<ARRaycastHit>();
-        Vector2 screenPosition = new Vector2 (Screen.width / 2, Screen.height / 2);
+        /*LANZAR UN RAYO DESDE EL CENTRO DE LA PANTALLA*/
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();//Lista: "Lista<>" para almacenar el listado de los rayos
 
-        aRRaycastManager.Raycast(screenPosition, hits, TrackableType.Planes);
+        //             ....    = new vector2 (El ancho de nuestra pantalla dividida en 2, La altura de nuestra pantalal dividida en 2)
+        Vector2 screenPosition = new Vector2 (Screen.width / 2, Screen.height / 2);//El punto central de pantalla en las cordenadas x: "vector2"
+
+        aRRaycastManager.Raycast(screenPosition, hits, TrackableType.Planes);//Lanzar el aRRaycast: (El pto. donde se lanza el rayo, lista, es porque usamos los planos)
         
+        /*SI EL RAYO TOCA UN PLANO ACTIVAMOS EL INDICADOR PARA ALERTAR AL USUARIO*/
         if(hits.Count > 0)
         {
             this.transform.position = hits[0].pose.position;
             this.transform.rotation = hits[0].pose.rotation;
 
-            indicator.SetActive(true);
+            indicator.SetActive(true);//Activar nuestro indicador
         }
         else
         {
-            indicator.SetActive(false);
+            indicator.SetActive(false);//Si no estamos mirando un plano valido, desactivamos el indicador
         }
     }
     //_____________________:::::________________________
