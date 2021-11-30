69f48fd977cdb6b228e97b1ec88e429b019378f8
Yo o <joel.huillca@alumnos.ucn.cl>
2021-11-30 17:32:35 -0300

    Estado cero

---
+++ PPA-El-18/Assets/Integrantes/JOel H,/EstadoCero.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EstadoCero : MonoBehaviour
+{
+
+    public GameObject desactivar1;
+    void Start()
+    {
+        desactivar1.SetActive(false);
+    }
+
+ 
+}
